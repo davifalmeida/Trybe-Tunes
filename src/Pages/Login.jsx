98f326e23a6074a261d5a05ad5f8fe663c8385fb
Davi <davifalmeida@gmail.com>
2022-08-18 13:06:26 -0300

    Requesito 2 :fireworks:

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,18 +1,81 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+
+const minLength = 3;
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        <h1>Login</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
+state = {
+  userMinLength: false,
+  userName: '',
+  userPassword: '',
+  loading: false,
+  login: false,
+}
+
+  handleUserName = ({ target }) => {
+    const { value } = target;
+    if (value.length >= minLength) {
+      this.setState({
+        userMinLength: true,
+        userName: value,
+        userPassword: value });
+    } else {
+      this.setState({
+        userMinLength: false,
+      });
+    }
   }
+  // implementar em handleUserName a logica de userName e userPassword devem ser as duas liberadas para o login
+
+   login = async () => {
+     const { userName, userPassword } = this.state;
+     this.setState({ loading: true }, async () => {
+       await createUser({ name: userName, password: userPassword });
+       this.setState({ login: true });
+     });
+   }
+
+   render() {
+     const { userMinLength, loading, login } = this.state;
+
+     return (
+       <div data-testid="page-login">
+         {loading === false ? (
+           <div>
+             <h1>Login</h1>
+             <form>
+               <input
+                 type="text"
+                 name="userName"
+                 placeholder="Username"
+                 onChange={ this.handleUserName }
+                 data-testid="login-name-input"
+               />
+               <input
+                 type="password"
+                 name="password"
+                 placeholder="Password"
+                 onChange={ this.handleUserName }
+               />
+               <button
+                 type="submit"
+                 disabled={ !userMinLength }
+                 onClick={ this.login }
+                 data-testid="login-submit-button"
+               >
+                 Entrar
+               </button>
+             </form>
+           </div>
+         ) : (
+           <div>
+             {login === true ? <Redirect to="/search" /> : <h1> Carregando... </h1> }
+           </div>
+         )}
+       </div>
+     );
+   }
 }
 
 export default Login;

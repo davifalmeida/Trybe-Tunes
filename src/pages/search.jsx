b92fa86bff71685d775072f602bc434e22373626
Davi <davifalmeida@gmail.com>
2022-08-18 16:33:26 -0300

    Requesito 5

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import Header from '../Components/header';
 
 class Search extends React.Component {
+  state = {
+    search: false,
+  }
+
+  handleSearch = ({ target }) => {
+    const { value } = target;
+    if (value.length >= 2) this.setState({ search: true });
+  };
+
   render() {
+    const { search } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
         <form>
-          <input type="text" placeholder="Search" />
-          <button type="submit">Search</button>
+          <input
+            type="text"
+            placeholder="Nome do artista"
+            data-testid="search-artist-input"
+            onChange={ this.handleSearch }
+          />
+          <button
+            type="submit"
+            disabled={ !search }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
         </form>
       </div>
     );
